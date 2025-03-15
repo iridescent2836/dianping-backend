20ef753cb7cee88622bd0249793fa854e250c0d2
niconicad <1756208746@qq.com>
2025-03-15 17:19:37 +0800

    version 1.0.1

--- Web/src/main/java/com/SoftwareEngineeringGroup2/entity/User.java
+++ Web/src/main/java/com/SoftwareEngineeringGroup2/entity/User.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "用户实体类")
 @Entity
 @Table(name = "users")
 @Data
@@ -13,15 +15,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class User {
+    @Schema(description = "用户ID", example = "1")
     @Id
     @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Schema(description = "用户名", example = "zhangsan")
     @Column(name = "user_name")
     private String username;
 
+    @Schema(description = "用户邮箱", example = "zhangsan@example.com")
     @Column(name = "user_email")
     private String email;
-
 }
